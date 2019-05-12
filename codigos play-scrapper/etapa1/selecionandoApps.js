dc70f8c86d7f7b4d209e2e7ce03f18dcdf31e9e7
luizfernando1996 <luizfernando1996@yahoo.com.br>
2019-05-12 01:56:39 -0300

    Versão 1.2
    Estudos de como efetuar o processo de
    obtenção do objetivo do artigo.

--- codigos play-scrapper/etapa1/selecionandoApps.js
+++ codigos play-scrapper/etapa1/selecionandoApps.js
@@ -1,26 +1,27 @@
 var gplay = require('google-play-scraper');
 
-var palavraChave = [
+var palavraChave = [ //um array que contêm todas as palavras chaves
     'programming',
     'programação',
     'learn programming',
     'ensino de programação',
     'learn code'
 ]
+
 var indice2 = 0
 
-for (indice = 0; indice < palavraChave.length; indice++) {
-    gplay.search({       //search retorna um array de aplicativos
-        term: palavraChave[indice],
+for (indice = 0; indice < palavraChave.length; indice++) {//coleta uma lista de aplicativos para cada palavra chave
+    gplay.search({       //O método search retorna um array de aplicativos
+        term: palavraChave[indice],//palavra chave será o termo a ser pesquisado
         lang: "pt-br",
-        num: 250 //número máximo aceitado
+        num: 250 //número máximo de aplicativos aceitado pela ferramenta em cada lista de aplicativos
     }).then(aplicativosSelecionados, console.log).then(incrementarValorIndice);
 }
 function aplicativosSelecionados(values) {
     console.log("______________________________________________________________")
     console.log("Nessa pesquisa a palavra chave utilizada foi: " + palavraChave[indice2])
     for (x = 0; x < 250; x++) {
-        console.log(values[x].title) //acessando o array de aplicativos
+        console.log(values[x].title) //apresentando o titulo do aplicativo na lista dos aplicativos
     }
 }
 function incrementarValorIndice() {
