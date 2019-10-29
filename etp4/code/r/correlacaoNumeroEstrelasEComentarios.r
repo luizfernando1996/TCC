26dfe646c32d65cb643ca5a5c4eb47a1e4c8c3ad
Luiz <luizfernando1996@yahoo.com.br>
2019-10-29 16:49:14 -0300

    R atualizado
    Código em r atualizado

--- etp4/code/r/correlacaoNumeroEstrelasEComentarios.r
+++ etp4/code/r/correlacaoNumeroEstrelasEComentarios.r
@@ -1,8 +1,12 @@
 
-#----------------------------------LEITURA E TRATAMENTO DO ARQUIVO DE MÉTRICAS-------------------
+#----------------------------------LEITURA E TRATAMENTO DO ARQUIVO DE COMENTARIOS-------------------
 
-#Seta o diretório
-setwd("C:\\Users\\Jonathan\\Desktop\\Sistemas de informa��o\\8�Periodo\\TCCII\\C�digo Coleta Node\\TCC\\etp4\\entrada\\EtapaAnterior")
+#Seta o diretorio
+
+#caminhoJonathan <- "C:\\Users\\Jonathan\\Desktop\\Sistemas de informa��o\\8�Periodo\\TCCII\\C�digo Coleta Node\\TCC\\etp4\\entrada\\EtapaAnterior"
+caminhoLuiz <- "C:\\Users\\Lenovo\\node_modules\\TCC\\etp4\\entrada\\Processado"
+
+setwd(caminhoLuiz)
 getwd()
 
 #Atrbui os dados do arquivo para um dataframe
@@ -11,14 +15,14 @@ View(df)
 
 #---------------------------------Número de Instalações X Estrelas---------------------------------
 
-#O teste de correla��o demonstra um correla��o aproximada de 0,39
-#O que demonstra correla��o fraca, dessa forma iremos gerar gr�ficos
+#O teste de correlacao demonstra um correlacao aproximada de 0,39
+#O que demonstra correlacao fraca, dessa forma iremos gerar graficos.
 
 cor.test(df$numero.de.estrelas.do.app,df$estatistica.do.aplicativo, method="pearson")
 cor.test(df$numero.de.estrelas.do.app,df$estatistica.do.aplicativo, method="spearman")
 cor.test(df$numero.de.estrelas.do.app,df$estatistica.do.aplicativo, method="kendall")
 
-#Gráfico de regresão
+#Grafico de regressao
 # z = plot(df$numero.de.estrelas.do.app,df$estatistica.do.aplicativo)
 # regressao = lm(df$numero.de.estrelas.do.app~df$estatistica.do.aplicativo)
 # abline(regressao)
