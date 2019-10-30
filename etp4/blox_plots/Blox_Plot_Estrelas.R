3e46ebd79ecc393314ea27b4a1287877e4d2a153
Luiz <luizfernando1996@yahoo.com.br>
2019-10-30 17:04:56 -0300

    Auto stash before merge of "master" and "origin/master"

--- etp4/blox_plots/Blox_Plot_Estrelas.R
+++ etp4/blox_plots/Blox_Plot_Estrelas.R
@@ -1,5 +1,8 @@
-#Seta o diretório
-setwd("C:\\Users\\Jonathan\\Desktop\\Sistemas de informação\\8°Periodo\\TCCII\\Código Coleta Node\\TCC\\etp4\\entrada\\Processado")
+#Seta o diretï¿½rio
+
+#diretorioJonathan = "C:\\Users\\Jonathan\\Desktop\\Sistemas de informaï¿½ï¿½o\\8ï¿½Periodo\\TCCII\\Cï¿½digo Coleta Node\\TCC\\etp4\\entrada\\Processado"
+diretorioLuiz = "C:\\Users\\Lenovo\\node_modules\\TCC\\etp4\\entrada\\Processado"
+setwd(diretorioLuiz)
 getwd()
 
 #Atrbui os dados do arquivo para um dataframe
@@ -8,10 +11,11 @@ df <-read.csv("ResultadoTCC.txt", sep=";", encoding = "UTF-8")
 #Cria vetores com as colunas do dataframe
 vetor_grupos <- as.numeric(df[,2])
 vetor_estrelas <- as.numeric(df[,3])
-vetor_grupos <- as.numeric(df[,4])
+vetor_estatisticaComentarioAplicativo <- as.numeric(df[,4])
 
 
 boxplot(vetor_estrelas~vetor_grupos)
 
+boxplot(vetor_estatisticaComentarioAplicativo~vetor_grupos)
 
 
