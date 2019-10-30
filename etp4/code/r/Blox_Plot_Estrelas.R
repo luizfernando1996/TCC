9938ad0626f8f7081deeb1392115f25c84ab9ab9
Luiz <luizfernando1996@yahoo.com.br>
2019-10-30 17:21:57 -0300

    Melhorias nos comentários e na localização dos arquivos

--- etp4/code/r/Blox_Plot_Estrelas.R
+++ etp4/code/r/Blox_Plot_Estrelas.R
@@ -1,7 +1,8 @@
-#Seta o diret�rio
-
+#Caminho dos diretorios
 #diretorioJonathan = "C:\\Users\\Jonathan\\Desktop\\Sistemas de informa��o\\8�Periodo\\TCCII\\C�digo Coleta Node\\TCC\\etp4\\entrada\\Processado"
 diretorioLuiz = "C:\\Users\\Lenovo\\node_modules\\TCC\\etp4\\entrada\\Processado"
+
+#Seta o diretorio
 setwd(diretorioLuiz)
 getwd()
 
@@ -13,9 +14,8 @@ vetor_grupos <- as.numeric(df[,2])
 vetor_estrelas <- as.numeric(df[,3])
 vetor_estatisticaComentarioAplicativo <- as.numeric(df[,4])
 
-
+#Plota o boxplot com as variaveis
 boxplot(vetor_estrelas~vetor_grupos)
-
 boxplot(vetor_estatisticaComentarioAplicativo~vetor_grupos)
 
 
